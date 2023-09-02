66674dbd2fb4d2237e34ffa6681f87c2fc063d5d
Paul Brendel <pcbrendel@gmail.com>
2023-09-02 11:20:22 -0400

    move evans data

--- R/expit.R
+++
@@ -1,5 +0,0 @@
-# Inverse logit (expit) function
-
-expit <- function(p) {
-  exp(p) / (1 + exp(p))
-}
--- test.r
+++ test.r
@@ -3,6 +3,8 @@ library(tidyverse)
 library(available)
 library(roxygen2)
 
+install.packages("languageserver")
+
 use_data_raw()
 use_mit_license()
 
@@ -19,8 +21,3 @@ create_package("C:/Users/brend/Desktop/folder/multibias")
 
 # release to CRAN
 use_release_issue()
-
-
-install.packages("languageserver")
-
-c(1, 2, 3)
