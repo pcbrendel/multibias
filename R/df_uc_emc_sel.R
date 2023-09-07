23bd756944284419098b463060eb4e677e72cc06
Paul Brendel <pcbrendel@gmail.com>
2023-09-07 11:09:22 -0400

    updated documentation for df_uc_emc_sel

--- R/df_uc_emc_sel.R
+++ R/df_uc_emc_sel.R
@@ -3,11 +3,12 @@
 #'
 #' Data containing three sources of bias, three known confounders, and
 #'  100,000 observations. This data is obtained by sampling with replacement
-#'  with probability = S from df_uc_emc_source then removing the columns X, U,
-#'  and S. The resulting data corresponds to what a researcher would see in the
-#'  real-world: a misclassified exposure, Xstar; missing data on a confounder U;
-#'  and missing data for those not selected into the study (S=0). As seen
-#'  in df_uc_emc_source, the true, unbiased exposure-outcome odds ratio = 2.
+#'  with probability = S from \code{df_uc_emc_source} then removing the columns
+#'  X, U, and S. The resulting data corresponds to what a researcher would
+#'  see in the real-world: a misclassified exposure, Xstar; missing data on
+#'  a confounder U; and missing data for those not selected into the study
+#'  (S=0). As seen in \code{df_uc_emc_source}, the true, unbiased
+#'  exposure-outcome odds ratio = 2.
 #'
 #' @format A dataframe with 100,000 rows and 5 variables:
 #' \describe{
