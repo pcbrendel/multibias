6fdfebf4b2ca5a324f07dc124f5a63b82d040846
Paul Brendel <pcbrendel@gmail.com>
2023-09-09 14:35:41 -0400

    add adjust_uc

---
+++ R/adjust_uc.R
@@ -0,0 +1,204 @@
+#' Adust for uncontrolled confounding.
+#'
+#' \code{adjust_uc} returns the exposure-outcome odds ratio and confidence
+#' interval, adjusted for uncontrolled confounding.
+#'
+#' description
+#'
+#' @inheritParams adjust_emc_sel
+#' @param u_model_coefs The regression coefficients corresponding to the model:
+#'  \ifelse{html}{\out{logit(P(U=1)) = &alpha;<sub>0</sub> +
+#'  &alpha;<sub>1</sub>X + &alpha;<sub>2</sub>Y +
+#'  &alpha;<sub>2+j</sub>C<sub>j</sub>, } where U is the (binary) unmeasured
+#'  confounder, X is the (binary) exposure, Y is the (binary) outcome,
+#'  C represents the vector of (binary) measured confounders (if any),
+#'  and j corresponds to the number of measured confounders.
+#'  The number of parameters therefore equals 3 + j.}{\eqn{logit(P(U=1)) =}}
+#'
+#' @examples
+#' adjust_uc(
+#'  evans,
+#'  exposure = "SMK",
+#'  outcome = "CHD",
+#'  confounders = "AGE",
+#'  u_model_coefs = c(0.10, 0.10, 0.10, 0.02),
+#' )
+#'
+#' @import dplyr
+#' @importFrom magrittr %>%
+#' @importFrom stats binomial
+#' @importFrom stats glm
+#' @importFrom stats qnorm
+#' @importFrom stats rbinom
+#' @importFrom stats plogis
+#' @importFrom rlang .data
+#'
+#' @export
+
+adjust_uc <- function(
+  data,
+  exposure,
+  outcome,
+  confounders = NULL,
+  u_model_coefs,
+  level = 0.95
+) {
+
+  n <- nrow(data)
+  len_c <- length(confounders)
+  len_u_coefs <- length(u_model_coefs)
+
+  x <- data[, exposure]
+  y <- data[, outcome]
+
+  if (sum(x %in% c(0, 1)) != n) {
+    stop("Exposure must be a binary integer.")
+  }
+  if (sum(y %in% c(0, 1)) != n) {
+    stop("Outcome must be a binary integer.")
+  }
+  if (len_u_coefs != 3 + len_c) {
+    stop(
+      paste0(
+        "Incorrect length of U model coefficients. ",
+        "Length should equal 3 + number of confounders."
+      )
+    )
+  }
+
+  u1_0     <- u_model_coefs[1]
+  u1_x     <- u_model_coefs[2]
+  u1_y     <- u_model_coefs[3]
+
+  if (is.null(confounders)) {
+
+    df <- data.frame(X = x, Y = y)
+    df$Upred <- rbinom(n, 1, plogis(u1_0 + u1_x * df$X + u1_y * df$Y))
+
+    final <- glm(
+      Y ~ X + Upred,
+      family = binomial(link = "logit"),
+      data = df
+    )
+
+    est <- summary(final)$coef[2, 1]
+    se <- summary(final)$coef[2, 2]
+    alpha <- 1 - level
+
+    return(
+      list(
+        exp(est),
+        c(
+          exp(est + se * qnorm(alpha / 2)),
+          exp(est + se * qnorm(1 - alpha / 2))
+        )
+      )
+    )
+
+  } else if (len_c == 1) {
+
+    c1 <- data[, confounders]
+    df <- data.frame(X = x, Y = y, C1 = c1)
+
+    u1_c1 <- u_model_coefs[4]
+
+    df$Upred <- rbinom(n, 1, plogis(u1_0 + u1_x * df$X + u1_y * df$Y +
+                                      u1_c1 * df$C1))
+
+    final <- glm(
+      Y ~ X + C1 + Upred,
+      family = binomial(link = "logit"),
+      data = df
+    )
+
+    est <- summary(final)$coef[2, 1]
+    se <- summary(final)$coef[2, 2]
+    alpha <- 1 - level
+
+    return(
+      list(
+        exp(est),
+        c(
+          exp(est + se * qnorm(alpha / 2)),
+          exp(est + se * qnorm(1 - alpha / 2))
+        )
+      )
+    )
+
+  } else if (len_c == 2) {
+
+    c1 <- data[, confounders[1]]
+    c2 <- data[, confounders[2]]
+
+    df <- data.frame(X = x, Y = y, C1 = c1, C2 = c2)
+
+    u1_c1 <- u_model_coefs[4]
+    u1_c2 <- u_model_coefs[5]
+
+    df$Upred <- rbinom(n, 1, plogis(u1_0 + u1_x * df$X + u1_y * df$Y +
+                                      u1_c1 * df$C1 + u1_c2 * df$C2))
+
+    final <- glm(
+      Y ~ X + C1 + C2 + Upred,
+      family = binomial(link = "logit"),
+      data = df
+    )
+
+    est <- summary(final)$coef[2, 1]
+    se <- summary(final)$coef[2, 2]
+    alpha <- 1 - level
+
+    return(
+      list(
+        exp(est),
+        c(
+          exp(est + se * qnorm(alpha / 2)),
+          exp(est + se * qnorm(1 - alpha / 2))
+        )
+      )
+    )
+
+  } else if (len_c == 3) {
+
+    c1 <- data[, confounders[1]]
+    c2 <- data[, confounders[2]]
+    c3 <- data[, confounders[3]]
+
+    df <- data.frame(X = x, Y = y, C1 = c1, C2 = c2, C3 = c3)
+
+    u1_c1 <- u_model_coefs[4]
+    u1_c2 <- u_model_coefs[5]
+    u1_c3 <- u_model_coefs[6]
+
+    df$Upred <- rbinom(
+      n, 1,
+      plogis(
+        u1_0 + u1_x * df$X + u1_y * df$Y +
+          u1_c1 * df$C1 + u1_c2 * df$C2 + u1_c3 * df$C3
+      )
+    )
+
+    final <- glm(
+      Y ~ X + C1 + C2 + C3 + Upred,
+      family = binomial(link = "logit"),
+      data = df
+    )
+
+    est <- summary(final)$coef[2, 1]
+    se <- summary(final)$coef[2, 2]
+    alpha <- 1 - level
+
+    return(
+      list(
+        exp(est),
+        c(
+          exp(est + se * qnorm(alpha / 2)),
+          exp(est + se * qnorm(1 - alpha / 2))
+        )
+      )
+    )
+
+  } else if (len_c > 3) {
+    stop("This function is currently not compatible with >3 confounders.")
+  }
+}
