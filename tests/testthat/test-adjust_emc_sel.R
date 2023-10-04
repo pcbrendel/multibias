9835fcc29ed1ee3a94a0a43136f1909f7a862541
Paul Brendel <pcbrendel@gmail.com>
2023-10-04 08:39:57 -0400

    update tests

--- tests/testthat/test-adjust_emc_sel.R
+++ tests/testthat/test-adjust_emc_sel.R
@@ -1,17 +1,67 @@
-results <- adjust_emc_sel(
+nobias_model <- glm(Y ~ X + C1,
+                    family = binomial(link = "logit"),
+                    data = df_emc_sel_source)
+
+x_model <- glm(X ~ Xstar + Y + C1,
+               family = binomial(link = "logit"),
+               data = df_emc_sel_source)
+s_model <- glm(S ~ Xstar + Y + C1,
+               family = binomial(link = "logit"),
+               data = df_emc_sel_source)
+
+single_run <- adjust_emc_sel(
   df_emc_sel,
   exposure = "Xstar",
   outcome = "Y",
   confounders = "C1",
-  x_model_coefs = c(-2.78, 1.62, 0.58, 0.34),
-  s_model_coefs = c(0.04, 0.18, 0.92, 0.05)
+  x_model_coefs = c(
+    x_model$coef[1],
+    x_model$coef[2],
+    x_model$coef[3],
+    x_model$coef[4]
+  ),
+  s_model_coefs = c(
+    s_model$coef[1],
+    s_model$coef[2],
+    s_model$coef[3],
+    s_model$coef[4]
+  )
 )
 
+n <- 100000
+nreps <- 10
+est <- vector()
+for (i in 1:nreps) {
+  bdf <- df_emc_sel[sample(seq_len(n), n, replace = TRUE), ]
+  results <- adjust_emc_sel(
+    bdf,
+    exposure = "Xstar",
+    outcome = "Y",
+    confounders = "C1",
+    x_model_coefs = c(
+      x_model$coef[1],
+      x_model$coef[2],
+      x_model$coef[3],
+      x_model$coef[4]
+    ),
+    s_model_coefs = c(
+      s_model$coef[1],
+      s_model$coef[2],
+      s_model$coef[3],
+      s_model$coef[4]
+    )
+  )
+  est[i] <- results$estimate
+}
+
+or_true <- exp(summary(nobias_model)$coef[2, 1])
+or_adjusted <- median(est)
+
 test_that("odds ratio and confidence interval output", {
-  expect_gt(results$estimate, 1.5)
-  expect_lt(results$estimate, 2.5)
+  expect_gt(or_adjusted, or_true - 0.15)
+  expect_lt(or_adjusted, or_true + 0.15)
   expect_vector(
-    results$ci,
+    single_run$ci,
     ptype = double(),
     size = 2
   )
