6e7392e3d812aa46dc97689d424ce024b0206a3b
Paul Brendel <pcbrendel@gmail.com>
2023-10-06 08:29:59 -0400

    update single bias tests

--- tests/testthat/test-adjust_sel.R
+++ tests/testthat/test-adjust_sel.R
@@ -1,14 +1,50 @@
-results <- adjust_sel(
-  evans,
-  exposure = "SMK",
-  outcome = "CHD",
-  confounders = "HPT",
-  s_model_coefs = c(qlogis(0.25), log(0.75), log(0.75))
+nobias_model <- glm(Y ~ X + C1,
+                    family = binomial(link = "logit"),
+                    data = df_sel_source)
+
+s_model <- glm(S ~ X + Y,
+               family = binomial(link = "logit"),
+               data = df_sel_source)
+
+single_run <- adjust_sel(
+  df_sel,
+  "X",
+  "Y",
+  "C1",
+  s_model_coefs = c(
+    s_model$coef[1],
+    s_model$coef[2],
+    s_model$coef[3]
+  )
 )
 
+n <- 100000
+nreps <- 10
+est <- vector()
+for (i in 1:nreps) {
+  bdf <- df_sel[sample(seq_len(n), n, replace = TRUE), ]
+  results <- adjust_sel(
+    s1df,
+    "X",
+    "Y",
+    "C1",
+    s_model_coefs = c(
+      s_model$coef[1],
+      s_model$coef[2],
+      s_model$coef[3]
+    )
+  )
+  est[i] <- results$estimate
+}
+
+or_true <- exp(summary(nobias_model)$coef[2, 1])
+or_adjusted <- median(est)
+
 test_that("odds ratio and confidence interval output", {
+  expect_gt(or_adjusted, or_true - 0.15)
+  expect_lt(or_adjusted, or_true + 0.15)
   expect_vector(
-    results$ci,
+    single_run$ci,
     ptype = double(),
     size = 2
   )
--- tests/testthat/test-adjust_uc.R
+++ tests/testthat/test-adjust_uc.R
@@ -1,14 +1,52 @@
-results <- adjust_uc(
-  evans,
-  exposure = "SMK",
-  outcome = "CHD",
-  confounders = "AGE",
-  u_model_coefs = c(qlogis(0.25), log(0.5), log(2.5), log(2)),
+nobias_model <- glm(Y ~ X + C1 + U,
+                    family = binomial(link = "logit"),
+                    data = df_uc_source)
+
+u_model <- glm(U ~ X + Y + C1,
+               family = binomial(link = "logit"),
+               data = df_uc_source)
+
+single_run <- adjust_uc(
+  df,
+  "X",
+  "Y",
+  "C1",
+  u_model_coefs = c(
+    u_model$coef[1],
+    u_model$coef[2],
+    u_model$coef[3],
+    u_model$coef[4]
+  )
 )
 
+n <- 100000
+nreps <- 10
+est <- vector()
+for (i in 1:nreps) {
+  bdf <- df_uc[sample(seq_len(n), n, replace = TRUE), ]
+  results <- adjust_uc(
+    bdf,
+    exposure = "X",
+    outcome = "Y",
+    confounders = "C1",
+    u_model_coefs = c(
+      u_model$coef[1],
+      u_model$coef[2],
+      u_model$coef[3],
+      u_model$coef[4]
+    )
+  )
+  est[i] <- results$estimate
+}
+
+or_true <- exp(summary(nobias_model)$coef[2, 1])
+or_adjusted <- median(est)
+
 test_that("odds ratio and confidence interval output", {
+  expect_gt(or_adjusted, or_true - 0.15)
+  expect_lt(or_adjusted, or_true + 0.15)
   expect_vector(
-    results$ci,
+    single_run$ci,
     ptype = double(),
     size = 2
   )
