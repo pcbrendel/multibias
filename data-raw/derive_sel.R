4689dbe9013898c7e5d097b234318f6afc58cd01
pbrendel <pbrendel@valohealth.com>
2023-12-06 09:23:12 -0500

    3 confs for sel dfs, docs, testing

--- data-raw/derive_sel.R
+++ data-raw/derive_sel.R
@@ -7,17 +7,21 @@ n <- 100000
 
 # DERIVE DATA
 c1 <- rbinom(n, 1, 0.5)
-x <- rbinom(n, 1, plogis(-2 + log(1.5) * c1))
-y <- rbinom(n, 1, plogis(-2.5 + log(2) * x + log(1.5) * c1))
+c2 <- rbinom(n, 1, 0.2)
+c3 <- rbinom(n, 1, 0.8)
+x <- rbinom(n, 1, plogis(-2 + log(1.5) * c1 + log(0.75) * c2 +
+                           log(2.5) * c3))
+y <- rbinom(n, 1, plogis(-2.5 + log(2) * x + log(1.5) * c1 - log(2.5) * c2 -
+                           log(0.75) * c3))
 s <- rbinom(n, 1, plogis(log(2.5) * x + log(2.5) * y))
 
-df <- data.frame(X = x, Y = y, C1 = c1, S = s)
+df <- data.frame(X = x, Y = y, C1 = c1, C2 = c2, C3 = c3, S = s)
 s1df <- df[sample(seq_len(n), size = n, replace = TRUE, prob = df$S), ]
 
-rm(c1, x, y, s)
+rm(c1, c2, c3, x, y, s)
 
 # INSPECT MODELS
-nobias_model <- glm(Y ~ X + C1,
+nobias_model <- glm(Y ~ X + C1 + C2 + C3,
                     family = binomial(link = "logit"),
                     data = df)
 
@@ -26,9 +30,9 @@ c(exp(summary(nobias_model)$coef[2, 1] +
         summary(nobias_model)$coef[2, 2] * qnorm(.025)),
   exp(summary(nobias_model)$coef[2, 1] +
         summary(nobias_model)$coef[2, 2] * qnorm(.975)))
-# 1.97 (1.87, 2.07)
+# 1.94 (1.86, 2.02)
 
-bias_model <- glm(Y ~ X + C1,
+bias_model <- glm(Y ~ X + C1 + C2 + C3,
                   family = binomial(link = "logit"),
                   data = s1df)
 
@@ -37,7 +41,7 @@ c(exp(summary(bias_model)$coef[2, 1] +
         summary(bias_model)$coef[2, 2] * qnorm(.025)),
   exp(summary(bias_model)$coef[2, 1] +
         summary(bias_model)$coef[2, 2] * qnorm(.975)))
-# 1.63 (1.57, 1.70)
+# 1.58 (1.52, 1.63)
 
 # OBTAIN BIAS PARAMETERS
 s_model <- glm(S ~ X + Y,
@@ -49,22 +53,22 @@ adjust_sel(
   s1df,
   "X",
   "Y",
-  "C1",
+  c("C1", "C2", "C3"),
   s_model_coefs = c(
     s_model$coef[1],
     s_model$coef[2],
     s_model$coef[3]
   )
 )
-# 1.93 (1.86, 2.00)
+# 1.88 (1.82, 1.93)
 
 # CREATE PACKAGE DATA
 df_sel_source <- df
 head(df_sel_source)
-use_data(df_sel_source)
+use_data(df_sel_source, overwrite = TRUE)
 
 row.names(s1df) <- NULL
 df_sel <- s1df %>%
-  select(X, Y, C1) # only have access to these in real-world
+  select(X, Y, C1, C2, C3) # only have access to these in real-world
 head(df_sel)
 use_data(df_sel, overwrite = TRUE)
--- tests/testthat/test-adjust_sel.R
+++ tests/testthat/test-adjust_sel.R
@@ -1,4 +1,7 @@
-nobias_model <- glm(Y ~ X + C1,
+set.seed(1234)
+# 0 confounders
+
+nobias_model <- glm(Y ~ X,
                     family = binomial(link = "logit"),
                     data = df_sel_source)
 
@@ -8,9 +11,8 @@ s_model <- glm(S ~ X + Y,
 
 single_run <- adjust_sel(
   df_sel,
-  "X",
-  "Y",
-  "C1",
+  exposure = "X",
+  outcome = "Y",
   s_model_coefs = c(
     s_model$coef[1],
     s_model$coef[2],
@@ -25,9 +27,8 @@ for (i in 1:nreps) {
   bdf <- df_sel[sample(seq_len(n), n, replace = TRUE), ]
   results <- adjust_sel(
     bdf,
-    "X",
-    "Y",
-    "C1",
+    exposure = "X",
+    outcome = "Y",
     s_model_coefs = c(
       s_model$coef[1],
       s_model$coef[2],
@@ -41,8 +42,170 @@ or_true <- exp(summary(nobias_model)$coef[2, 1])
 or_adjusted <- median(est)
 
 test_that("odds ratio and confidence interval output", {
-  expect_gt(or_adjusted, or_true - 0.15)
-  expect_lt(or_adjusted, or_true + 0.15)
+  expect_gt(or_adjusted, or_true - 0.1)
+  expect_lt(or_adjusted, or_true + 0.1)
+  expect_vector(
+    single_run$ci,
+    ptype = double(),
+    size = 2
+  )
+})
+
+# 1 confounder
+
+nobias_model <- glm(Y ~ X + C1,
+                    family = binomial(link = "logit"),
+                    data = df_sel_source)
+
+s_model <- glm(S ~ X + Y,
+               family = binomial(link = "logit"),
+               data = df_sel_source)
+
+single_run <- adjust_sel(
+  df_sel,
+  exposure = "X",
+  outcome = "Y",
+  confounders = "C1",
+  s_model_coefs = c(
+    s_model$coef[1],
+    s_model$coef[2],
+    s_model$coef[3]
+  )
+)
+
+n <- 100000
+nreps <- 10
+est <- vector()
+for (i in 1:nreps) {
+  bdf <- df_sel[sample(seq_len(n), n, replace = TRUE), ]
+  results <- adjust_sel(
+    bdf,
+    exposure = "X",
+    outcome = "Y",
+    confounders = "C1",
+    s_model_coefs = c(
+      s_model$coef[1],
+      s_model$coef[2],
+      s_model$coef[3]
+    )
+  )
+  est[i] <- results$estimate
+}
+
+or_true <- exp(summary(nobias_model)$coef[2, 1])
+or_adjusted <- median(est)
+
+test_that("odds ratio and confidence interval output", {
+  expect_gt(or_adjusted, or_true - 0.1)
+  expect_lt(or_adjusted, or_true + 0.1)
+  expect_vector(
+    single_run$ci,
+    ptype = double(),
+    size = 2
+  )
+})
+
+# 2 confounders
+
+nobias_model <- glm(Y ~ X + C1 + C2,
+                    family = binomial(link = "logit"),
+                    data = df_sel_source)
+
+s_model <- glm(S ~ X + Y,
+               family = binomial(link = "logit"),
+               data = df_sel_source)
+
+single_run <- adjust_sel(
+  df_sel,
+  exposure = "X",
+  outcome = "Y",
+  confounders = c("C1", "C2"),
+  s_model_coefs = c(
+    s_model$coef[1],
+    s_model$coef[2],
+    s_model$coef[3]
+  )
+)
+
+n <- 100000
+nreps <- 10
+est <- vector()
+for (i in 1:nreps) {
+  bdf <- df_sel[sample(seq_len(n), n, replace = TRUE), ]
+  results <- adjust_sel(
+    bdf,
+    exposure = "X",
+    outcome = "Y",
+    confounders = c("C1", "C2"),
+    s_model_coefs = c(
+      s_model$coef[1],
+      s_model$coef[2],
+      s_model$coef[3]
+    )
+  )
+  est[i] <- results$estimate
+}
+
+or_true <- exp(summary(nobias_model)$coef[2, 1])
+or_adjusted <- median(est)
+
+test_that("odds ratio and confidence interval output", {
+  expect_gt(or_adjusted, or_true - 0.1)
+  expect_lt(or_adjusted, or_true + 0.1)
+  expect_vector(
+    single_run$ci,
+    ptype = double(),
+    size = 2
+  )
+})
+
+# 3 confounders
+
+nobias_model <- glm(Y ~ X + C1 + C2 + C3,
+                    family = binomial(link = "logit"),
+                    data = df_sel_source)
+
+s_model <- glm(S ~ X + Y,
+               family = binomial(link = "logit"),
+               data = df_sel_source)
+
+single_run <- adjust_sel(
+  df_sel,
+  exposure = "X",
+  outcome = "Y",
+  confounders = c("C1", "C2", "C3"),
+  s_model_coefs = c(
+    s_model$coef[1],
+    s_model$coef[2],
+    s_model$coef[3]
+  )
+)
+
+n <- 100000
+nreps <- 10
+est <- vector()
+for (i in 1:nreps) {
+  bdf <- df_sel[sample(seq_len(n), n, replace = TRUE), ]
+  results <- adjust_sel(
+    bdf,
+    exposure = "X",
+    outcome = "Y",
+    confounders = c("C1", "C2", "C3"),
+    s_model_coefs = c(
+      s_model$coef[1],
+      s_model$coef[2],
+      s_model$coef[3]
+    )
+  )
+  est[i] <- results$estimate
+}
+
+or_true <- exp(summary(nobias_model)$coef[2, 1])
+or_adjusted <- median(est)
+
+test_that("odds ratio and confidence interval output", {
+  expect_gt(or_adjusted, or_true - 0.1)
+  expect_lt(or_adjusted, or_true + 0.1)
   expect_vector(
     single_run$ci,
     ptype = double(),
