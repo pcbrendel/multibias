ea7c51484fe31906cddff57c5fb06aefca27f84b
pbrendel <pbrendel@valohealth.com>
2024-03-03 14:00:43 -0500

    update uc_sel to three known confounders

--- R/df_uc_sel_source.R
+++ R/df_uc_sel_source.R
@@ -5,7 +5,7 @@
 #'  \code{df_uc_sel} and can be used to obtain bias parameters for purposes
 #'  of validating the simultaneous multi-bias adjustment method with
 #'  \code{df_uc_sel}. With this source data, the fitted regression
-#'  \ifelse{html}{\out{logit(P(Y=1)) = &alpha;<sub>0</sub> + &alpha;<sub>1</sub>X + &alpha;<sub>2</sub>C1 + &alpha;<sub>3</sub>U}}{\eqn{logit(P(Y=1)) = \alpha_0 + \alpha_1 X + \alpha_2 C1 + \alpha_3 U}}
+#'  \ifelse{html}{\out{logit(P(Y=1)) = &alpha;<sub>0</sub> + &alpha;<sub>1</sub>X + &alpha;<sub>2</sub>C1 + &alpha;<sub>3</sub>C2 + &alpha;<sub>4</sub>C3 + &alpha;<sub>5</sub>U}}{\eqn{logit(P(Y=1)) = \alpha_0 + \alpha_1 X + \alpha_2 C1 + \alpha_3 C2 + \alpha_4 C3 + \alpha_5 U}}
 #'  shows that the true, unbiased exposure-outcome odds ratio = 2.
 #'
 #' @format A dataframe with 100,000 rows and 5 columns:
@@ -13,6 +13,8 @@
 #'     \item{X}{true exposure, 1 = present and 0 = absent}
 #'     \item{Y}{outcome, 1 = present and 0 = absent}
 #'     \item{C1}{1st confounder, 1 = present and 0 = absent}
+#'     \item{C2}{2nd confounder, 1 = present and 0 = absent}
+#'     \item{C3}{3rd confounder, 1 = present and 0 = absent}
 #'     \item{U}{unmeasured confounder, 1 = present and 0 = absent}
 #'     \item{S}{selection, 1 = selected into the study and 0 = not selected into the study}
 #' }
--- data-raw/derive_uc_sel.R
+++ data-raw/derive_uc_sel.R
@@ -7,18 +7,22 @@ n <- 100000
 
 # DERIVE DATA
 c1 <- rbinom(n, 1, 0.5)
+c2 <- rbinom(n, 1, 0.2)
+c3 <- rbinom(n, 1, 0.8)
 u <- rbinom(n, 1, .5)
-x <- rbinom(n, 1, plogis(-2 + log(1.5) * c1 + log(3) * u))
-y <- rbinom(n, 1, plogis(-2.5 + log(2) * x + log(1.5) * c1 + log(3) * u))
+x <- rbinom(n, 1, plogis(-2 + log(1.5) * c1 + log(0.75) * c2 +
+                           log(2.5) * c3 + log(2) * u))
+y <- rbinom(n, 1, plogis(-2.5 + log(2) * x + log(1.5) * c1 - log(2.5) * c2 -
+                           log(0.75) * c3 + log(2) * u))
 s <- rbinom(n, 1, plogis(log(2.5) * x + log(2.5) * y))
 
-df <- data.frame(X = x, Y = y, C1 = c1, U = u, S = s)
+df <- data.frame(X = x, Y = y, C1 = c1, C2 = c2, C3 = c3, U = u, S = s)
 s1df <- df[sample(seq_len(n), size = n, replace = TRUE,
                   prob = df$S), ]
-rm(c1, u, x, y, s)
+rm(c1, c2, c3, u, x, y, s)
 
 # INSPECT MODELS
-nobias_model <- glm(Y ~ X + C1 + U,
+nobias_model <- glm(Y ~ X + C1 + C2 + C3 + U,
                     family = binomial(link = "logit"),
                     data = df)
 
@@ -27,9 +31,9 @@ c(exp(summary(nobias_model)$coef[2, 1] +
         summary(nobias_model)$coef[2, 2] * qnorm(.025)),
   exp(summary(nobias_model)$coef[2, 1] +
         summary(nobias_model)$coef[2, 2] * qnorm(.975)))
-# 2.02 (1.95, 2.09)
+# 1.99 (1.93, 2.07)
 
-bias_model <- glm(Y ~ X + C1,
+bias_model <- glm(Y ~ X + C1 + C2 + C3,
                   family = binomial(link = "logit"),
                   data = s1df)
 
@@ -38,10 +42,10 @@ c(exp(summary(bias_model)$coef[2, 1] +
         summary(bias_model)$coef[2, 2] * qnorm(.025)),
   exp(summary(bias_model)$coef[2, 1] +
         summary(bias_model)$coef[2, 2] * qnorm(.975)))
-# 2.24 (2.17, 2.31)
+# 1.87 (1.81, 1.93)
 
 # OBTAIN BIAS PARAMETERS
-u_model <- glm(U ~ X + Y + C1,
+u_model <- glm(U ~ X + Y + C1 + C2 + C3,
                data = df,
                family = binomial(link = "logit"))
 s_model <- glm(S ~ X + Y,
@@ -53,12 +57,14 @@ adjust_uc_sel(
   s1df,
   "X",
   "Y",
-  "C1",
+  c("C1", "C2", "C3"),
   u_model_coefs = c(
     u_model$coef[1],
     u_model$coef[2],
     u_model$coef[3],
-    u_model$coef[4]
+    u_model$coef[4],
+    u_model$coef[5],
+    u_model$coef[6]
   ),
   s_model_coefs = c(
     s_model$coef[1],
@@ -66,15 +72,15 @@ adjust_uc_sel(
     s_model$coef[3]
   )
 )
-# 2.01 (1.96, 2.07)
+# 2.00 (1.95, 2.06)
 
 # CREATE PACKAGE DATA
 df_uc_sel_source <- df
 head(df_uc_sel_source)
-use_data(df_uc_sel_source)
+use_data(df_uc_sel_source, overwrite = TRUE)
 
 row.names(s1df) <- NULL
 df_uc_sel <- s1df %>%
-  select(X, Y, C1) # only have access to these in real-world
+  select(X, Y, C1, C2, C3) # only have access to these in real-world
 head(df_uc_sel)
 use_data(df_uc_sel, overwrite = TRUE)
