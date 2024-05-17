624062489930f6a2522f50e8821f9d5ba4cf1985
pbrendel <pbrendel@valohealth.com>
2024-05-17 16:59:51 -0600

    update uc with cont exposure outcome

--- data-raw/derive_uc.R
+++ data-raw/derive_uc.R
@@ -4,23 +4,38 @@ library(tidyverse)
 
 set.seed(1234)
 n <- 100000
+effect_strength <- 2
 
 # DERIVE DATA
 c1 <- rbinom(n, 1, 0.5)
 c2 <- rbinom(n, 1, 0.2)
 c3 <- rbinom(n, 1, 0.8)
 u <- rbinom(n, 1, 0.5)
-x <- rbinom(n, 1, plogis(-2 + log(1.5) * c1 + log(0.75) * c2 +
-                           log(2.5) * c3 + log(2) * u))
-y <- rbinom(n, 1, plogis(-2.5 + log(2) * x + log(1.5) * c1 - log(2.5) * c2 -
-                           log(0.75) * c3 + log(2) * u))
+x_bi <- rbinom(n, 1, plogis(-2 + log(1.5) * c1 + log(0.75) * c2 +
+                              log(2.5) * c3 + log(2) * u))
+x_cont <- -2 + 1.5 * c1 + 0.75 * c2 + 2.5 * c3 + 2 * u + rnorm(n, 0, .5)
+y_bi <- rbinom(n, 1, plogis(-2.5 + log(effect_strength) * x_bi + log(1.5) * c1 -
+                              log(2.5) * c2 - log(0.75) * c3 + log(2) * u))
+y_cont <- (
+  -2.5 + effect_strength * x_cont + 1.5 * c1 - 2.5 * c2 - 0.75 * c3 + 2 * u +
+    rnorm(n, 0, .5)
+)
 
-df <- data.frame(X = x, Y = y, C1 = c1, C2 = c2, C3 = c3, U = u)
+df <- data.frame(
+  X_bi = x_bi,
+  X_cont = round(x_cont, 2),
+  Y_bi = y_bi,
+  Y_cont = round(y_cont, 2),
+  C1 = c1,
+  C2 = c2,
+  C3 = c3,
+  U = u
+)
 
-rm(c1, c2, c3, u, x, y)
+rm(c1, c2, c3, u, x_bi, x_cont, y_bi, y_cont)
 
 # INSPECT MODELS
-nobias_model <- glm(Y ~ X + C1 + C2 + C3 + U,
+nobias_model <- glm(Y_bi ~ X_bi + C1 + C2 + C3 + U,
                     family = binomial(link = "logit"),
                     data = df)
 
@@ -29,9 +44,9 @@ c(exp(summary(nobias_model)$coef[2, 1] +
         summary(nobias_model)$coef[2, 2] * qnorm(.025)),
   exp(summary(nobias_model)$coef[2, 1] +
         summary(nobias_model)$coef[2, 2] * qnorm(.975)))
-# 2.00 (1.93, 2.07)
+# 2.03 (1.94, 2.10)
 
-bias_model <- glm(Y ~ X + C1 + C2 + C3,
+bias_model <- glm(Y_bi ~ X_bi + C1 + C2 + C3,
                   family = binomial(link = "logit"),
                   data = df)
 
@@ -40,18 +55,18 @@ c(exp(summary(bias_model)$coef[2, 1] +
         summary(bias_model)$coef[2, 2] * qnorm(.025)),
   exp(summary(bias_model)$coef[2, 1] +
         summary(bias_model)$coef[2, 2] * qnorm(.975)))
-# 2.22 (2.15, 2.30)
+# 2.26 (2.18, 2.34)
 
 # OBTAIN BIAS PARAMETERS
-u_model <- glm(U ~ X + Y + C1 + C2 + C3,
+u_model <- glm(U ~ X_bi + Y_bi + C1 + C2 + C3,
                data = df,
                family = binomial(link = "logit"))
 
 # ADJUST
-adjust_uc(
+est <- adjust_uc(
   df,
-  "X",
-  "Y",
+  "X_bi",
+  "Y_bi",
   c("C1", "C2", "C3"),
   u_model_coefs = c(
     u_model$coef[1],
@@ -62,7 +77,8 @@ adjust_uc(
     u_model$coef[6]
   )
 )
-# 1.99 (1.92, 2.06)
+
+between(est$estimate, 1.9, 2.1)
 
 # CREATE PACKAGE DATA
 df_uc_source <- df
@@ -70,6 +86,6 @@ head(df_uc_source)
 use_data(df_uc_source, overwrite = TRUE)
 
 df_uc <- df %>%
-  select(X, Y, C1, C2, C3) # only have access to these in real-world
+  select(-U) # no access to this in the real world
 head(df_uc)
 use_data(df_uc, overwrite = TRUE)
