f68d7df5f6847700187042042d1fe392dac66bf6
Paul Brendel <pcbrendel@gmail.com>
2024-06-19 13:48:00 -0700

    3 confounders in uc_emc data

--- R/df_uc_emc_source.R
+++ R/df_uc_emc_source.R
@@ -8,11 +8,13 @@
 #'  \ifelse{html}{\out{logit(P(Y=1)) = &alpha;<sub>0</sub> + &alpha;<sub>1</sub>X + &alpha;<sub>2</sub>C1 + &alpha;<sub>3</sub>U}}{\eqn{logit(P(Y=1)) = \alpha_0 + \alpha_1 X + \alpha_2 C1 + \alpha_3 U}}
 #'  shows that the true, unbiased exposure-outcome odds ratio = 2.
 #'
-#' @format A dataframe with 100,000 rows and 5 columns:
+#' @format A dataframe with 100,000 rows and 7 columns:
 #' \describe{
 #'     \item{X}{true exposure, 1 = present and 0 = absent}
 #'     \item{Y}{outcome, 1 = present and 0 = absent}
 #'     \item{C1}{1st confounder, 1 = present and 0 = absent}
+#'     \item{C2}{2nd confounder, 1 = present and 0 = absent}
+#'     \item{C3}{3rd confounder, 1 = present and 0 = absent}
 #'     \item{U}{unmeasured confounder, 1 = present and 0 = absent}
 #'     \item{Xstar}{misclassified exposure, 1 = present and 0 = absent}
 #' }
