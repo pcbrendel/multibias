d703c1af2cf3d7ef903bf3bca3c2609ff380b9bb
Paul Brendel <pcbrendel@gmail.com>
2024-09-24 15:19:18 -0700

    create new causal data class

---
+++ R/causal_data.R
@@ -0,0 +1,102 @@
+#' Class for Observed Data
+#'
+
+
+data_observed <- function(
+    data,
+    exposure,
+    outcome,
+    confounders = NULL,
+    remove = NULL) {
+  stopifnot(
+    is.data.frame(data),
+    is.character(exposure) & length(exposure) == 1,
+    is.character(outcome) & length(outcome) == 1,
+    is.character(confounders) | is.null(confounders)
+  )
+
+  obj <- list(
+    data = data[, !colnames(data) %in% remove],
+    exposure = exposure,
+    outcome = outcome,
+    confounders = confounders
+  )
+
+  class(obj) <- "data_observed"
+  return(obj)
+}
+
+print.data_observed <- function(x, ...) {
+  cat("Observed Data\n")
+  cat("------------------\n")
+  cat("Exposure:", x$exposure, "\n")
+  cat("Outcome:", x$outcome, "\n")
+  if (!is.null(x$confounders)) {
+    cat("Confounders:", paste(x$confounders, collapse = ", "), "\n")
+  }
+  cat("Data head: \n")
+  print(head(x$data))
+}
+
+
+#' Class for Validation Data
+#'
+
+
+data_validation <- function(
+    data,
+    true_exposure,
+    true_outcome,
+    confounders = NULL,
+    misclassified_exposure = NULL,
+    misclassified_outcome = NULL,
+    selection = NULL,
+    remove = NULL) {
+  stopifnot(
+    is.data.frame(data),
+    is.character(true_exposure) & length(true_exposure) == 1,
+    is.character(true_outcome) & length(true_outcome) == 1,
+    is.character(confounders) | is.null(confounders),
+    (is.character(misclassified_exposure) &
+      length(misclassified_exposure) == 1) |
+      is.null(misclassified_exposure),
+    (is.character(misclassified_outcome) &
+      length(misclassified_outcome) == 1) |
+      is.null(misclassified_outcome),
+    (is.character(selection) & length(selection) == 1) | is.null(selection)
+  )
+
+  obj <- list(
+    data = data[, !colnames(data) %in% remove],
+    true_exposure = true_exposure,
+    true_outcome = true_outcome,
+    confounders = confounders,
+    misclassified_exposure = misclassified_exposure,
+    misclassified_outcome = misclassified_outcome,
+    selection = selection
+  )
+
+  class(obj) <- "data_validation"
+  return(obj)
+}
+
+print.data_validation <- function(x, ...) {
+  cat("Validation Data\n")
+  cat("------------------\n")
+  cat("True exposure:", x$true_exposure, "\n")
+  cat("True outcome:", x$true_outcome, "\n")
+  if (!is.null(x$confounders)) {
+    cat("Confounders:", paste(x$confounders, collapse = ", "), "\n")
+  }
+  if (!is.null(x$misclassified_exposure)) {
+    cat("Misclassified exposure:", x$misclassified_exposure, "\n")
+  }
+  if (!is.null(x$misclassified_outcome)) {
+    cat("Misclassified outcome:", x$misclassified_outcome, "\n")
+  }
+  if (!is.null(x$selection)) {
+    cat("Selection:", x$selection, "\n")
+  }
+  cat("Data head: \n")
+  print(head(x$data))
+}
