ca1bf4cc04d4e703e43c3f3a9843aae74d777d44
pcbrendel <pcbrendel@gmail.com>
2025-03-28 19:45:09 -0700

    testing for om and sel

--- tests/testthat/test-adjust_om.R
+++ tests/testthat/test-adjust_om.R
@@ -1,5 +1,5 @@
 set.seed(1234)
-n <- 10000
+n <- 20000
 nreps <- 10
 
 # cont X just for testing that function runs
@@ -21,18 +21,17 @@ y_model <- glm(
 
 df_observed <- data_observed(
   df_om,
+  bias = "om",
   exposure = "X_cont",
   outcome = "Ystar",
   confounders = NULL
 )
+list_for_om <- list(y = as.vector(coef(y_model)))
+bp_om <- bias_params(coef_list = list_for_om)
 
 single_run <- adjust_om(
   df_observed,
-  y_model_coefs = c(
-    y_model$coef[1],
-    y_model$coef[2],
-    y_model$coef[3]
-  )
+  bias_params = bp_om
 )
 
 est <- vector()
@@ -40,145 +39,14 @@ for (i in 1:nreps) {
   bdf <- df_om[sample(seq_len(n), n, replace = TRUE), ]
   df_observed <- data_observed(
     bdf,
+    bias = "om",
     exposure = "X",
     outcome = "Ystar",
     confounders = NULL
   )
   results <- adjust_om(
     df_observed,
-    y_model_coefs = c(
-      y_model$coef[1],
-      y_model$coef[2],
-      y_model$coef[3]
-    )
-  )
-  est[i] <- results$estimate
-}
-
-or_true <- exp(summary(nobias_model)$coef[2, 1])
-or_adjusted <- median(est)
-
-test_that("odds ratio and confidence interval output", {
-  expect_gt(or_adjusted, or_true - 0.1)
-  expect_lt(or_adjusted, or_true + 0.1)
-  expect_vector(
-    single_run$ci,
-    ptype = double(),
-    size = 2
-  )
-})
-
-# 1 confounder
-
-nobias_model <- glm(Y ~ X + C1,
-  family = binomial(link = "logit"),
-  data = df_om_source
-)
-
-y_model <- glm(Y ~ X + Ystar + C1,
-  family = binomial(link = "logit"),
-  data = df_om_source
-)
-
-df_observed <- data_observed(
-  df_om,
-  exposure = "X_cont",
-  outcome = "Ystar",
-  confounders = "C1"
-)
-
-single_run <- adjust_om(
-  df_observed,
-  y_model_coefs = c(
-    y_model$coef[1],
-    y_model$coef[2],
-    y_model$coef[3],
-    y_model$coef[4]
-  )
-)
-
-est <- vector()
-for (i in 1:nreps) {
-  bdf <- df_om[sample(seq_len(n), n, replace = TRUE), ]
-  df_observed <- data_observed(
-    bdf,
-    exposure = "X",
-    outcome = "Ystar",
-    confounders = "C1"
-  )
-  results <- adjust_om(
-    df_observed,
-    y_model_coefs = c(
-      y_model$coef[1],
-      y_model$coef[2],
-      y_model$coef[3],
-      y_model$coef[4]
-    )
-  )
-  est[i] <- results$estimate
-}
-
-or_true <- exp(summary(nobias_model)$coef[2, 1])
-or_adjusted <- median(est)
-
-test_that("odds ratio and confidence interval output", {
-  expect_gt(or_adjusted, or_true - 0.1)
-  expect_lt(or_adjusted, or_true + 0.1)
-  expect_vector(
-    single_run$ci,
-    ptype = double(),
-    size = 2
-  )
-})
-
-# 2 confounders
-
-nobias_model <- glm(Y ~ X + C1 + C2,
-  family = binomial(link = "logit"),
-  data = df_om_source
-)
-
-y_model <- glm(Y ~ X + Ystar + C1 + C2,
-  family = binomial(link = "logit"),
-  data = df_om_source
-)
-
-df_observed <- data_observed(
-  df_om,
-  exposure = "X_cont",
-  outcome = "Ystar",
-  confounders = c("C1", "C2")
-)
-
-single_run <- adjust_om(
-  df_observed,
-  y_model_coefs = c(
-    y_model$coef[1],
-    y_model$coef[2],
-    y_model$coef[3],
-    y_model$coef[4],
-    y_model$coef[5]
-  )
-)
-
-est <- vector()
-for (i in 1:nreps) {
-  bdf <- df_om[sample(seq_len(n), n, replace = TRUE), ]
-  df_observed <- data_observed(
-    bdf,
-    exposure = "X",
-    outcome = "Ystar",
-    confounders = c("C1", "C2")
-  )
-  results <- adjust_om(
-    df_observed,
-    y_model_coefs = c(
-      y_model$coef[1],
-      y_model$coef[2],
-      y_model$coef[3],
-      y_model$coef[4],
-      y_model$coef[5]
-    )
+    bias_params = bp_om
   )
   est[i] <- results$estimate
 }
@@ -210,21 +78,17 @@ y_model <- glm(Y ~ X + Ystar + C1 + C2 + C3,
 
 df_observed <- data_observed(
   df_om,
+  bias = "om",
   exposure = "X_cont",
   outcome = "Ystar",
   confounders = c("C1", "C2", "C3")
 )
+list_for_om <- list(y = as.vector(coef(y_model)))
+bp_om <- bias_params(coef_list = list_for_om)
 
 single_run <- adjust_om(
   df_observed,
-  y_model_coefs = c(
-    y_model$coef[1],
-    y_model$coef[2],
-    y_model$coef[3],
-    y_model$coef[4],
-    y_model$coef[5],
-    y_model$coef[6]
-  )
+  bias_params = bp_om
 )
 
 est <- vector()
@@ -232,20 +96,14 @@ for (i in 1:nreps) {
   bdf <- df_om[sample(seq_len(n), n, replace = TRUE), ]
   df_observed <- data_observed(
     bdf,
+    bias = "om",
     exposure = "X",
     outcome = "Ystar",
     confounders = c("C1", "C2", "C3")
   )
   results <- adjust_om(
     df_observed,
-    y_model_coefs = c(
-      y_model$coef[1],
-      y_model$coef[2],
-      y_model$coef[3],
-      y_model$coef[4],
-      y_model$coef[5],
-      y_model$coef[6]
-    )
+    bias_params = bp_om
   )
   est[i] <- results$estimate
 }
@@ -268,6 +126,7 @@ test_that("odds ratio and confidence interval output", {
 or_val <- adjust_om(
   data_observed = data_observed(
     df_om,
+    bias = "om",
     exposure = "X",
     outcome = "Ystar",
     confounders = c("C1", "C2", "C3")
