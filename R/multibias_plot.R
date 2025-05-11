4280e41d0b5af47d14f9a69318c3e645d7287b94
pcbrendel <pcbrendel@gmail.com>
2025-05-11 18:52:30 -0400

    plot function updates

--- R/multibias_plot.R
+++ R/multibias_plot.R
@@ -1,13 +1,35 @@
 multibias_plot <- function(
-  data_observed,
-  multibias_result
-) {
-  observed_est <- unname(summary(df_observed)[2, 2])
-  observed_se <- unname(summary(df_observed)[2, 3])
+    data_observed,
+    multibias_result) {
+  summary_quietly <- quietly(summary)
+  observed_summary <- summary_quietly(data_observed)$result
+
+  observed_est <- observed_summary[[2]][2]
+  observed_se <- observed_summary[[3]][2]
   adjusted_est <- multibias_result$estimate
   adjusted_se <- multibias_result$std.error
 
-  final <- x
+  xmin <- min(observed_est - observed_se * 4, adjusted_est - adjusted_se * 4)
+  xmax <- max(observed_est + observed_se * 4, adjusted_est + adjusted_se * 4)
+
+  final <- ggplot() +
+    xlim(xmin, xmax) +
+    stat_function(
+      fun = dnorm,
+      args = list(mean = observed_est, sd = observed_se),
+      aes(color = "Observed")
+    ) +
+    stat_function(
+      fun = dnorm,
+      args = list(mean = adjusted_est, sd = adjusted_se),
+      aes(color = "Adjusted")
+    ) +
+    labs(
+      title = "Observed vs. Adjusted Estimates",
+      x = "Effect Estimate",
+      y = "Density",
+    ) +
+    theme_bw()
 
   return(final)
 }
